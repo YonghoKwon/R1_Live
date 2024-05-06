16a57799cc73d5f893c96f4b1c4e0750023d6898
Grandes <ho2708@naver.com>
2024-05-06 21:57:01 +0900

    feature(study) : ~섹션2 Actor #2까지 실습

---
+++ R1/Source/R1/GameMode/R1GameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "R1GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API AR1GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ R1/Source/R1/System/R1GameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "R1GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API UR1GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
