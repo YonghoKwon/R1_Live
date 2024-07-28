b8f549646dd8d6cbf7f633a87430952f7e323844
Grandes <ho2708@naver.com>
2024-07-28 10:53:34 +0900

    feature(study) : ~섹션5 델리게이트

--- R1/Source/R1/Character/R1Player.cpp
+++ R1/Source/R1/Character/R1Player.cpp
@@ -29,14 +29,45 @@ AR1Player::AR1Player()
 	Camera->bUsePawnControlRotation = false;  // Camera does not rotate relative to arm
 
 	GetMesh()->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -88.f), FRotator(0.f, -90.f, 0.f));
+
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	// GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
 }
 
 void AR1Player::BeginPlay()
 {
 	Super::BeginPlay();
+
+	GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBeginOverlap);
+
+	// Delegate
+	RookissDelegate.BindUObject(this, &ThisClass::TestFunc);
+
+	// 실행
+	RookissDelegate.Execute();
+
+	// 해제
+	RookissDelegate.Unbind();
+
+	auto h1 = RookissMulticastDelegate.AddUObject(this, &ThisClass::TestFunc);
+	RookissMulticastDelegate.AddUObject(this, &ThisClass::TestFunc);
+
+	// 실행
+	RookissMulticastDelegate.Broadcast();
+
+	RookissMulticastDelegate.Remove(h1);
+	// RookissMulticastDelegate.RemoveAll();
+
+
 }
 
 void AR1Player::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AR1Player::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("OnBeginOverlap"));
 }
