15d94507d2ed5e9c0a4d5a1a4cbc5d072db952a3
Grandes <ho2708@naver.com>
2024-10-05 23:27:20 +0900

    feature(study) : ~섹션8 UI#2

--- R1/Source/R1/Character/R1Character.cpp
+++ R1/Source/R1/Character/R1Character.cpp
@@ -3,6 +3,8 @@
 
 #include "Character/R1Character.h"
 #include "R1Define.h"
+#include "Components/WidgetComponent.h"
+#include "UI/R1HpBarWidget.h"
 
 // Sets default values
 AR1Character::AR1Character()
@@ -10,13 +12,25 @@ AR1Character::AR1Character()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	HpBarComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HealthBar"));
+	HpBarComponent->SetupAttachment(RootComponent);
+
+	ConstructorHelpers::FClassFinder<UUserWidget> HealthBarWidget(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/Blueprints/UI/WBP_HpBar.WBP_HpBar_C'"));
+	if (HealthBarWidget.Succeeded())
+	{
+		HpBarComponent->SetWidgetClass(HealthBarWidget.Class);
+		HpBarComponent->SetWidgetSpace(EWidgetSpace::Screen);
+		HpBarComponent->SetRelativeLocation(FVector(0, 0, 130));
+		HpBarComponent->SetDrawSize(FVector2D(80, 10));
+		HpBarComponent->SetVisibility(true);
+	}
 }
 
 // Called when the game starts or when spawned
 void AR1Character::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	RefreshHpBarRatio();
 }
 
 // Called every frame
@@ -51,7 +65,8 @@ void AR1Character::OnDamaged(int32 Damage, TObjectPtr<AR1Character> Attacker)
 		OnDead(Attacker);
 	}
 
-	D(FString::Printf(TEXT("%d"), Hp));
+	//D(FString::Printf(TEXT("%d"), Hp));
+	RefreshHpBarRatio();
 }
 
 void AR1Character::OnDead(TObjectPtr<AR1Character> Attacker)
@@ -62,5 +77,18 @@ void AR1Character::OnDead(TObjectPtr<AR1Character> Attacker)
 	}
 
 	CreatureState = ECreatureState::Dead;
+}
+
+void AR1Character::RefreshHpBarRatio()
+{
+	if (HpBarComponent)
+	{
+		float Ratio = static_cast<float>(Hp) / MaxHp;
+		UR1HpBarWidget* HpBar = Cast<UR1HpBarWidget>(HpBarComponent->GetUserWidgetObject());
+		if (HpBar)
+		{
+			HpBar->SetHpRatio(Ratio);
+		}
+	}
 }
 
